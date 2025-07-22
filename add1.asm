eed066eacc13cbe74a1b6248c9f2de4a526103b2
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-07-23 01:52:40 +0530

    Update add1.asm

--- add1.asm
+++ add1.asm
@@ -19,37 +19,37 @@ section .text
     global _start
     
 _start:
-        rw 1,1,msg,msglen
-        mov rsi , numArray
-        mov ax ,00h
-        mov bx,0h
-        mov cx,5
+    rw 1,1,msg,msglen
+    mov rsi , numArray
+    mov ax ,00h
+    mov bx,0h
+    mov cx,5
         
-        up2: 
-            mov bl,byte[rsi]
-            add ax,bx
-            jnc skip
-            inc ah
+    up2: 
+        mov bl,byte[rsi]
+        add ax,bx
+        jnc skip
+        inc ah
             
-        skip:
-            inc rsi
-            dec cx
-            jnz up2
-            mov bp,4
+    skip:
+        inc rsi
+        dec cx
+        jnz up2
+        mov bp,4
             
-        up:
-            rol ax,4
-            mov bx,ax
-            and ax,0Fh
-            cmp al,09
-            jbe down
-            add al,07h
+    up:
+        rol ax,4
+        mov bx,ax
+        and ax,0Fh
+        cmp al,09
+        jbe down
+        add al,07h
             
-        down:
-            add al,30h
-            mov byte[temp],al
-            rw 1,1,temp,1
-            mov ax,bx
-            dec bp
-            jnz up
-            rw 60,0,0,0
+    down:
+        add al,30h
+        mov byte[temp],al
+        rw 1,1,temp,1
+        mov ax,bx
+        dec bp
+        jnz up
+        rw 60,0,0,0
